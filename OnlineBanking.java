40afcdf0a98afc1a58451473d76a52e7401ab31c
Nilufa khan <108535012+Nilufa-Khan@users.noreply.github.com>
2022-08-11 20:14:28 +0530

    Update some output statements

--- OnlineBanking.java
+++ OnlineBanking.java
@@ -35,9 +35,6 @@ public class OnlineBanking {
     }
     public float  transferFunds(int accountBalance,int transferAmount){
 
-        Random obj = new Random();
-        System.out.println("12 digit transfer account number ");
-        System.out.println(obj.nextLong(100000000000l,999999999999l));
         float availableBalance = 0.0f;
         if(transferAmount > accountBalance){
             availableBalance = accountBalance - transferAmount;
